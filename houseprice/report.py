2f8b4497e95eebc07b0b430a2cab8a187b8565be
lucabasa <luca.basanisi@gmail.com>
2019-09-02 22:27:15 +0200

    new plot_prediction function with diagonal line

--- houseprice/report.py
+++ houseprice/report.py
@@ -21,16 +21,34 @@ def print_scores(train, test, inf, oof, target):
     print(f'Test set MAE: {mean_absolute_error(test[target], oof)}')
     
 
-def plot_predictions(train, test, target, prediction, hue=None, legend=False, savename='test.png'):
+def plot_diagonal(ax):
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    low = min(xmin, xmax)
+    high = max(xmin, xmax)
+    scl = (high - low) / 100
+    
+    line = pd.DataFrame({'x': np.arange(low, high ,scl), # small hack for a diagonal line
+                         'y': np.arange(low, high ,scl)})
+    ax.plot(line.x, line.y, color='black', linestyle='--')
+    
+    return ax
+
+    
+def plot_predictions(train, test, true_label, pred_label, hue=None, legend=False, savename='test.png'):
+    
     fig, ax = plt.subplots(1,2, figsize=(15,6))
 
-    sns.scatterplot(x=target, y=prediction, data=train, 
+    sns.scatterplot(x=true_label, y=pred_label, data=train, 
                     hue=hue, ax=ax[0], legend=legend, alpha=0.7)
-    sns.scatterplot(x=target, y=prediction, data=test, 
+    sns.scatterplot(x=true_label, y=pred_label, data=test, 
                     hue=hue, ax=ax[1], legend=legend, alpha=0.7)
     
     ax[0].set_title('In fold prediction')
     ax[1].set_title('Oof prediction')
+    
+    ax[0] = plot_diagonal(ax[0])
+    ax[1] = plot_diagonal(ax[1])
 
     if not save_name.endswith('.png'):
         save_name += '.png'
