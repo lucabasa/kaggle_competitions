8607240128daecf33253318bef787401829f19ee
lucabasa <luca.basanisi@gmail.com>
2020-04-11 23:31:26 +0200

    prittier game plots

--- marc_madness/source/events_plot.py
+++ marc_madness/source/events_plot.py
@@ -1,5 +1,5 @@
 __author__ = 'lucabasa'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __status__ = 'development'
 
 
@@ -10,18 +10,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_game(data, year, day, w_team, l_team):
+def plot_game(data, all_names, year, day, w_team, l_team):
     
     fil = ((data.WTeamID == w_team) & 
            (data.LTeamID == l_team) & 
            (data.Season == year) & 
            (data.DayNum == day))
     
+    team1 = all_names[all_names.TeamID == w_team].TeamName.values[0]
+    team2 = all_names[all_names.TeamID == l_team].TeamName.values[0]
+    
     df = data[fil]
+    
+    textstr = '\n'.join((
+                f'Final Score: {df.WFinalScore.max()} - {df.LFinalScore.max()}',
+                f'Haltime score difference: {int(df.Halftime_difference.min())}',
+                f'Crunchtime score difference: {int(df["3mins_difference"].min())}', 
+                f'Lead Changes: {int(df.game_lc.min())}', 
+                f'Lead Changes in second half: {int(df.half2_lc.min())}', 
+                f'Lead Changes in final 3 minutes: {int(df.crunchtime_lc.min())}'))
+    if df.competitive.max() > 0:
+        textstr += '\nThe game was competitive'
+    else:
+        textstr += '\nThe game was not competitive'
     
     n_ot = df.n_OT.astype(int).max()
     
     fig, ax = plt.subplots(2,1,figsize=(18,14), facecolor='#f7f7f7')
+    fig.subplots_adjust(top=0.92)
+    fig.suptitle(f'{year} Season, Day {day}, {team1} - {team2}', fontsize=18)
     
     df.plot(x='ElapsedSeconds', y='WCurrentScore', ax=ax[0], label='Winner score', color='g')
     df.plot(x='ElapsedSeconds', y='LCurrentScore', ax=ax[0], label='Loser score', color='r')
@@ -36,6 +53,13 @@ def plot_game(data, year, day, w_team, l_team):
     
     ax[1].axhline(0, linestyle='--', color='r')
     ax[1].legend().set_visible(False)
+    ax[0].legend(loc='lower right')
+    
+    # place a text box in upper left in axes coords
+    props = dict(boxstyle='round', facecolor='silver', alpha=0.5)
+    ax[0].text(0.03, 0.97, textstr, transform=ax[0].transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+
     
     ax[0].annotate(df.Halftime_difference.max().astype(int),
             xy=(20*60, df[df.period==1][['WCurrentScore', 'LCurrentScore']].max().max()), 
@@ -45,6 +69,7 @@ def plot_game(data, year, day, w_team, l_team):
     for axes in ax:
         axes.axvline(20*60, linestyle='dotted', color='k')
         axes.set_xlabel('Seconds', fontsize=12)
+        axes.set_xlim((df.ElapsedSeconds.min(), df.ElapsedSeconds.max()))
         if n_ot > 0:
             for i in range(n_ot):
                 axes.axvline(40*60 + i*5*60, linestyle='dotted', color='k')
@@ -55,10 +80,10 @@ def plot_game(data, year, day, w_team, l_team):
     plt.show()
     
 
-def get_game(data, game_lc=False, half_lc=False, crunch_lc=False, 
+def get_game(data, names, game_lc=False, half_lc=False, crunch_lc=False, 
              half_score=False, crunch_score=False, final_score=False, 
              half_smaller=True, crunch_smaller=True, final_smaller=True,
-             use_competitive=False, competitive=True, OT=False, plot=True):
+             use_competitive=False, competitive=True, OT=False, plot=True, verbose=False):
     
     fil = (data.Season > 1)
     
@@ -108,18 +133,20 @@ def get_game(data, game_lc=False, half_lc=False, crunch_lc=False,
                 'Final_difference', 'n_OT', 'Halftime_difference', '3mins_difference', 
                 'game_lc', 'half2_lc', 'crunchtime_lc', 'competitive']
         final = df[cols].drop_duplicates().sample()
-        print(f'Season: {final.Season.min()}')
-        print(f'Day number: {final.DayNum.min()}')
-        print(f'Final Score: {final.WFinalScore.max()} - {final.LFinalScore.max()}')
-        print(f'Haltime score difference: {final.Halftime_difference.min()}')
-        print(f'Crunchtime score difference: {final["3mins_difference"].min()}')
-        print(f'Lead Changes: {final.game_lc.min()}')
-        print(f'Lead Changes in second half: {final.half2_lc.min()}')
-        print(f'Lead Changes in final 3 minutes: {final.crunchtime_lc.min()}')
-        if final.competitive.max() > 0:
-            print('The game was competitive')
-        else:
-            print('The game was not competitive')
+        if verbose:
+            print(f'Season: {final.Season.min()}')
+            print(f'Day number: {final.DayNum.min()}')
+            print(f'Final Score: {final.WFinalScore.max()} - {final.LFinalScore.max()}')
+            print(f'Haltime score difference: {final.Halftime_difference.min()}')
+            print(f'Crunchtime score difference: {final["3mins_difference"].min()}')
+            print(f'Lead Changes: {final.game_lc.min()}')
+            print(f'Lead Changes in second half: {final.half2_lc.min()}')
+            print(f'Lead Changes in final 3 minutes: {final.crunchtime_lc.min()}')
+            if final.competitive.max() > 0:
+                print('The game was competitive')
+            else:
+                print('The game was not competitive')
             
     if plot:
-        plot_game(data, final.Season.min(), final.DayNum.min(), final.WTeamID.min(), final.LTeamID.min())
+        plot_game(data, names, final.Season.min(), final.DayNum.min(), final.WTeamID.min(), final.LTeamID.min())
+        
