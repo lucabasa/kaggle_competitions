35e484c2663b1dea91db9ec672c53b7c24d86944
lucabasa <luca.basanisi@gmail.com>
2020-11-15 17:26:20 +0100

    training script

---
+++ mechanism_of_action/source/train.py
@@ -0,0 +1,36 @@
+__author__ = 'lucabasa'
+__version__ = '1.0.0'
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, num_features, num_targets, hidden_size):
+        super().__init__()
+        self.batch_norm1 = nn.BatchNorm1d(num_features)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dense1 = nn.utils.weight_norm(nn.Linear(num_features, hidden_size))
+        
+        self.batch_norm2 = nn.BatchNorm1d(hidden_size)
+        self.dropout2 = nn.Dropout(0.25)
+        self.dense2 = nn.utils.weight_norm(nn.Linear(hidden_size, hidden_size))
+        
+        self.batch_norm3 = nn.BatchNorm1d(hidden_size)
+        self.dropout3 = nn.Dropout(0.25)
+        self.dense3 = nn.utils.weight_norm(nn.Linear(hidden_size, num_targets))
+    
+    def forward(self, x):
+        x = self.batch_norm1(x)
+        x = self.dropout1(x)
+        x = F.leaky_relu(self.dense1(x), 1e-3)
+        
+        x = self.batch_norm2(x)
+        x = self.dropout2(x)
+        x = F.relu(self.dense2(x))
+        
+        x = self.batch_norm3(x)
+        x = self.dropout3(x)
+        x = self.dense3(x)
+        
+        return x
