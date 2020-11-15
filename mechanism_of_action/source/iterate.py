2593e30a020fe6a74434c3fd2ad37c47094ad215
lucabasa <luca.basanisi@gmail.com>
2020-11-15 10:26:13 +0100

    iterate over folds

--- mechanism_of_action/source/iterate.py
+++ mechanism_of_action/source/iterate.py
@@ -4,17 +4,46 @@ __version__ = '1.0.0'
 import numpy as np
 
 
-def it_seeds(train, target_cols, test, seeds, folds_func, verbose=False, **kwargs):
-    
-    oof = np.zeros((len(train), len(target_cols)))
-    predictions = np.zeros((len(test), len(target_cols)))
-
-    for seed in seeds:
-        if verbose:
-            print(f"SEED: {seed}")
-
-        oof_seed, predictions_seed = folds_func(seed, **kwargs)
-        oof += oof_seed / len(seeds)
-        predictions += predictions_seed / len(seeds)
+class Iterator():
+    def __init__(self, train, test, target_cols, seeds, n_folds, train_func, verbose=False, **kwargs):
+        self.train = train
+        self.test = test
+        self.target_cols = target_cols
+        self.seeds = seeds
+        self.n_folds = n_folds
+        self.train_func = train_func
+        self.train_args = kwargs
+        self.verbose = verbose
         
-    return oof, predictions
+
+    def it_seeds(self):
+
+        oof = np.zeros((len(self.train), len(self.target_cols)))
+        predictions = np.zeros((len(self.test), len(self.target_cols)))
+
+        for seed in self.seeds:
+            if self.verbose:
+                print(f"SEED: {seed}")
+
+            oof_seed, predictions_seed = self.it_folds(seed=seed)
+            oof += oof_seed / len(self.seeds)
+            predictions += predictions_seed / len(self.seeds)
+
+        return oof, predictions
+
+
+    def it_folds(self, seed):
+
+        oof = np.zeros((len(self.train), len(self.target_cols)))
+        predictions = np.zeros((len(self.test), len(self.target_cols)))
+
+        for fold in range(self.n_folds):
+            if self.verbose:
+                print(f'\t FOLD: {fold}')
+            
+            oof_fold, pred_fold = self.train_func(seed=seed, **self.train_args)
+
+            predictions += pred_fold / self.n_folds
+            oof += oof_fold
+
+        return oof, predictions
