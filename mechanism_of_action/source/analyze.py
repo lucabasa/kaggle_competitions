f1e32be34846e8f8ea0e92e67716c2de53e353ff
lucabasa <luca.basanisi@gmail.com>
2020-11-18 22:18:33 +0100

    more values plotted

--- mechanism_of_action/source/analyze.py
+++ mechanism_of_action/source/analyze.py
@@ -8,8 +8,8 @@ def plot_learning(train_losses, valid_losses, fold):
     
     fig, ax = plt.subplots(1,1, figsize=(12,8))
     
-    ax.plot(train_losses[3:], color='g')
-    ax.plot(valid_losses[3:], color='r')
+    ax.plot(train_losses[2:], color='g')
+    ax.plot(valid_losses[2:], color='r')
     
     plt.savefig(f'plots/learning_fold_{fold}.png')
     plt.close()
