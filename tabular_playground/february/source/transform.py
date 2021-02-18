1bf6d85a0d90226866f92f343cf5e75a551676c3
lucabasa <luca.basanisi@gmail.com>
2021-02-18 23:18:45 +0100

    target encoding

--- tabular_playground/february/source/transform.py
+++ tabular_playground/february/source/transform.py
@@ -115,4 +115,52 @@ class PCADf(BaseTransformer):
         X_tr = pd.DataFrame(X_tr, columns=self.original_columns)
         
         return X_tr
+
     
+class TargetEncoder(BaseTransformer):
+    # Heavily inspired by 
+    # https://github.com/MaxHalford/xam/blob/93c066990d976c7d4d74b63fb6fb3254ee8d9b48/xam/feature_extraction/encoding/bayesian_target.py#L8
+    def __init__(self, to_encode=None, prior_weight=100):
+        super().__init__()
+        if isinstance(to_encode, str):
+            self.to_encode = [to_encode]
+        else:
+            self.to_encode = to_encode
+        self.prior_weight = prior_weight
+        self.prior_ = None
+        self.posteriors_ = None
+     
+    
+    @reset_columns
+    def fit(self, X, y):
+        # Encode all categorical cols by default
+        if self.to_encode is None:
+            self.to_encode = [c for c in X if str(X[c].dtype)=='object' or str(X[c].dtype)=='category']
+        
+        tmp = X.copy()
+        tmp['target'] = y
+        
+        self.prior_ = tmp['target'].mean()
+        self.posteriors_ = {}
+        
+        for col in self.to_encode:
+            
+            agg = tmp.groupby(col)['target'].agg(['count', 'mean'])
+            counts = agg['count']
+            means = agg['mean']
+            pw = self.prior_weight
+            self.posteriors_[col] = ((pw * self.prior_ + counts * means) / (pw + counts)).to_dict()
+        
+        del tmp
+        return self
+    
+    
+    @self_columns
+    def transform(self, X, y=None):
+        
+        X_tr = X.copy()
+        
+        for col in self.to_encode:
+            X_tr[col] = X_tr[col].map(self.posteriors_[col]).fillna(self.prior_).astype(float)
+        
+        return X_tr
