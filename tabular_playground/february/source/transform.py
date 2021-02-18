178652834ab7ab2afc6cc5406c87289e5feffb6a
lucabasa <luca.basanisi@gmail.com>
2021-02-18 21:43:14 +0100

    pca added

--- tabular_playground/february/source/transform.py
+++ tabular_playground/february/source/transform.py
@@ -1,5 +1,5 @@
 __author__ = 'lucabasa'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __status__ = 'development'
 
 from tubesml.base import BaseTransformer, self_columns, reset_columns
@@ -71,4 +71,48 @@ class CatSimp(BaseTransformer):
         Xtransf = self.cat9_tr(Xtransf)
         
         return Xtransf
+    
+    
+class PCADf(BaseTransformer):
+    def __init__(self, n_components, svd_solver='auto', random_state=24, compress=False):
+        super().__init__()
+        self.n_components = n_components
+        self.svd_solver = svd_solver
+        self.n_components_ = 0
+        self.random_state = random_state
+        self.PCA = PCA(n_components=self.n_components, svd_solver=self.svd_solver, random_state=self.random_state)
+        self.compress = compress
+        self.original_columns = []
+        
+    @reset_columns
+    def fit(self, X, y=None):
+        
+        self.PCA.fit(X)
+        self.n_components_ = self.PCA.n_components_
+        
+        return self
+    
+    @self_columns
+    def transform(self, X, y=None):
+                
+        X_tr = self.PCA.transform(X)
+        X_tr = pd.DataFrame(X_tr, columns=[f'pca_{i}' for i in range(self.n_components_)])
+        
+        self.original_columns = X.columns
+        
+        if self.compress:
+            X_tr = self.inverse_transform(X_tr)
+        
+        return X_tr
+    
+    
+    def inverse_transform(self, X, y=None):
+        
+        try:
+            X_tr = self.PCA.inverse_transform(X)
+        except ValueError:
+            return X
+        X_tr = pd.DataFrame(X_tr, columns=self.original_columns)
+        
+        return X_tr
     
