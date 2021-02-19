10460feeff7ba69fed686de704a3bf1b2e13e18d
lucabasa <luca.basanisi@gmail.com>
2021-02-19 18:29:54 +0100

    report on predictions

---
+++ tabular_playground/february/source/report.py
@@ -0,0 +1,57 @@
+__author__ = 'lucabasa'
+__version__ = '1.0.0'
+__status__ = 'development'
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def _plot_diagonal(ax):
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    low = min(xmin, xmax)
+    high = max(xmin, xmax)
+    scl = (high - low) / 100
+    
+    line = pd.DataFrame({'x': np.arange(low, high ,scl), # small hack for a diagonal line
+                         'y': np.arange(low, high ,scl)})
+    ax.plot(line.x, line.y, color='black', linestyle='--')
+    
+    return ax
+
+
+def plot_regression_predictions(data, true_label, pred_label, hue=None, savename=None):
+    '''
+    Plot prediction vs true label.
+    '''
+    
+    tmp = data.copy()
+    tmp['Prediction'] = pred_label
+    tmp['True Label'] = true_label
+    
+    fig, ax = plt.subplots(1,2, figsize=(15,6))
+    
+    legend=False
+    addition=''
+    if hue is not None:
+        legend=True
+        addition = f' by {hue}'
+
+    sns.scatterplot(x='True Label', y='Prediction', data=tmp, ax=ax[0],
+                         hue=hue, legend=legend, alpha=0.5)
+    
+    ax[0] = _plot_diagonal(ax[0])
+    ax[0].set_title(f'True Label vs Prediction{addition}', fontsize=14)
+        
+    sns.histplot(data=tmp, x=true_label, kde=True, ax=ax[1], color='blue', label='True Label', alpha=0.4)
+    sns.histplot(data=tmp, x=pred_label, kde=True, ax=ax[1], color='red', label='Prediction', alpha=0.6)
+    ax[1].legend()
+    ax[1].set_xlabel('Target')
+    ax[1].set_title('Distribution of target and prediction', fontsize=14)
+    
+    if savename is not None:
+        plt.savefig('plots/' + savename)
+        plt.show()
+    else:
+        plt.show()
