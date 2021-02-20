b2949a5a8c028b8ced106144e870ac39770b6a78
lucabasa <luca.basanisi@gmail.com>
2021-02-20 16:15:19 +0100

    pdp introduction

--- tabular_playground/february/source/train.py
+++ tabular_playground/february/source/train.py
@@ -1,21 +1,37 @@
 __author__ = 'lucabasa'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __status__ = 'development'
 
 
 import pandas as pd
 import numpy as np
 from sklearn.base import clone
+from sklearn.inspection import partial_dependence
+from sklearn.exceptions import NotFittedError
 
 
-def train_model(train_df, test_df, target, trsf_pipe, estimator, cv, early_stopping=100, verbose=False):
+def get_pdp(clf, feature, data, fold):
+    val, exes = partial_dependence(clf, features=feature, 
+                                   X=data, grid_resolution=50, kind='average')
+    fold_tmp = pd.DataFrame({'x': exes[0], 'y': val[0]})
+    fold_tmp['feat'] = feature
+    fold_tmp['fold'] = fold + 1
+    
+    return fold_tmp
+
+
+def train_model(train_df, test_df, target, trsf_pipe, estimator, cv, early_stopping=100, verbose=False, pdp=None):
     
     oof = np.zeros(len(train_df))
     pred = np.zeros(len(test_df))
     train = train_df.copy()
     test = test_df.copy()
     
+    rep_res = {}
+    
     feat_df = pd.DataFrame()
+    feat_pdp = pd.DataFrame()
+    iteration = []
     
     for n_fold, (train_index, test_index) in enumerate(cv.split(train.values)):
         
@@ -42,6 +58,21 @@ def train_model(train_df, test_df, target, trsf_pipe, estimator, cv, early_stopp
         oof[test_index] = model.predict(val_set).ravel()
         pred += model.predict(test_set).ravel() / cv.get_n_splits()
         
+        #store iteration used
+        try:
+            iteration.append(model.best_iteration)
+        except AttributeError:
+            iteration.append(model.best_iteration_)
+        
+        # FIXME: pdp does not work with LightGBM
+        if pdp is not None:
+            for feat in pdp:
+                try:
+                    fold_tmp = get_pdp(model, feat, trn_set, n_fold)
+                    feat_pdp = pd.concat([feat_pdp, fold_tmp], axis=0)
+                except NotFittedError:
+                    break
+        
         # store feature importance
         fold_df = pd.DataFrame()
         fold_df['feat'] = trn_set.columns
@@ -52,6 +83,12 @@ def train_model(train_df, test_df, target, trsf_pipe, estimator, cv, early_stopp
     feat_df = feat_df.groupby(['feat'])['score'].agg(['mean', 'std'])
     feat_df = feat_df.sort_values(by=['mean'], ascending=False)
     feat_df['std'] = feat_df['std'] / np.sqrt(cv.get_n_splits() - 1)  # std of the mean, unbiased
+    
+    # pdp averaged over folds
+    feat_pdp = feat_pdp.groupby(['feat', 'x'])['y'].agg(['mean', 'std'])
+    
+    rep_res['feat_imp'] = feat_df
+    rep_res['n_iterations'] = iteration
+    rep_res['pdp'] = feat_pdp
 
-
-    return oof, pred, feat_df
+    return oof, pred, rep_res
